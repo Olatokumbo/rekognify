5e1780101625b2dd8615456cba63c7567cff2da3
Olatokumbo <faithodesola@gmail.com>
2024-11-30 19:56:19 +0000

    fix: remove amplify

--- amplify/functions/on-upload-handler/resource.ts
+++
@@ -1,7 +0,0 @@
-import { defineFunction } from '@aws-amplify/backend';
-import { projectName } from '../../constant';
-
-export const onUploadHandler = defineFunction({
-    name: `${projectName}`,
-    entry: "./index.ts",
-})
